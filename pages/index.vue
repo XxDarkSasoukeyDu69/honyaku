71e8b9b9c93774e1ca5939476faf018cc54ae5b1
lulmairine <alex02.cailler@gmail.com>
2020-06-09 18:48:02 +0200

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -45,11 +45,11 @@
         <b-col lg="6">
           <l-fab back-color="#54daa180" class="mb-5 "><font-awesome-icon icon="users" style="color: #1aae6f"/></l-fab>
           <l-typography h2 medium>Awesome features</l-typography>
-          <l-typography semi-light>Bienvenue sur la platforme indispensable, vous permettant de traduire vos text.</l-typography>
+          <l-typography semi-light>Bienvenue sur la platforme indispensable, vous permettant de traduire vos textes.</l-typography>
           <ul class="mt-5 pt-3">
-            <li class="d-flex align-items-center mb-3"><l-fab back-color="#54daa180" size="35"><font-awesome-icon icon="users" style="color: #1aae6f"/></l-fab><l-typography h5 bold class="mb-0 ml-3">Devener traducteur de text</l-typography></li>
-            <li class="d-flex align-items-center mb-3"><l-fab back-color="#54daa180" size="35"><font-awesome-icon icon="users" style="color: #1aae6f"/></l-fab><l-typography h5 bold class="mb-0 ml-3">Faites appelle a un traducteur</l-typography></li>
-            <li class="d-flex align-items-center mb-3"><l-fab back-color="#54daa180" size="35"><font-awesome-icon icon="users" style="color: #1aae6f"/></l-fab><l-typography h5 bold class="mb-0 ml-3">Traduisez vos text automatiquement</l-typography></li>
+            <li class="d-flex align-items-center mb-3"><l-fab back-color="#54daa180" size="35"><font-awesome-icon icon="users" style="color: #1aae6f"/></l-fab><l-typography h5 bold class="mb-0 ml-3">Devener traducteur de textes</l-typography></li>
+            <li class="d-flex align-items-center mb-3"><l-fab back-color="#54daa180" size="35"><font-awesome-icon icon="users" style="color: #1aae6f"/></l-fab><l-typography h5 bold class="mb-0 ml-3">Faites appelle à un traducteur</l-typography></li>
+            <li class="d-flex align-items-center mb-3"><l-fab back-color="#54daa180" size="35"><font-awesome-icon icon="users" style="color: #1aae6f"/></l-fab><l-typography h5 bold class="mb-0 ml-3">Traduisez vos texts automatiquement</l-typography></li>
           </ul>
         </b-col>
         <b-col lg="6">
