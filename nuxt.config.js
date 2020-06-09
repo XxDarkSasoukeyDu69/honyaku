e80cee3da8a64c37303d8654ac31ebf7e3fc01e8
lulmairine <alex02.cailler@gmail.com>
2020-06-09 16:43:56 +0200

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,11 +52,24 @@ export default {
   /*
   ** Build configuration
   */
+  env: {
+    baseUrl:
+      process.env.NODE_ENV === 'dev'
+        ? 'http://localhost:8000'
+        : 'https://api.honyaku.fr'
+  },
   build: {
     /*
     ** You can extend webpack config here
     */
     extend (config, ctx) {
+      config.module.rules.push({
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      })
     }
   }
 }
